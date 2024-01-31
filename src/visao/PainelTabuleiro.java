0acfd3909181c531d9a64c3ddb2a3b063d45913e
emerson-newart <emerson.art.n@hotmail.com>
2024-01-31 14:32:26 -0300

    Criando classes - Principal, PainelTabuleiro

---
+++ src/visao/PainelTabuleiro.java
@@ -0,0 +1,14 @@
+package visao;
+
+import javax.swing.JPanel;
+import modelo.Tabuleiro;
+
+/**
+ *
+ * @author emers
+ */
+public class PainelTabuleiro extends JPanel{
+    
+    public PainelTabuleiro(Tabuleiro tabuleiro){}
+    
+}
---
+++ src/visao/TelaPrincipal.java
@@ -0,0 +1,26 @@
+package visao;
+
+import javax.swing.JFrame;
+import modelo.Tabuleiro;
+
+/**
+ *
+ * @author Emerson Soares
+ */
+public class TelaPrincipal extends JFrame {
+
+    public TelaPrincipal() {
+        Tabuleiro tabuleiro = new Tabuleiro(16, 30, 50);
+        add(new PainelTabuleiro(tabuleiro));
+        setTitle("Campo Minado");
+        setSize(690, 438);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new TelaPrincipal();
+    }
+}
